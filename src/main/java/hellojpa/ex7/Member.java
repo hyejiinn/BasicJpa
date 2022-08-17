c87b45ef8825288328fb729b7e222ccd1be9595f
hyejin <hyejinbag772@gmail.com>
2022-08-17 15:32:31 +0900

    20220817 고급 매핑 - @MappedSuperclass

---
+++ src/main/java/hellojpa/ex7/Member.java
@@ -0,0 +1,17 @@
+package hellojpa.ex7;
+
+
+import javax.persistence.*;
+
+@Entity
+public class Member extends BaseEntity{
+
+    @Id
+    @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    @Column(name = "USERNAME")
+    private String username;
+
+}
